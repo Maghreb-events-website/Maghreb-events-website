4edfb9e085d9105d0cd4409088c3047929440c40
Jamie Datson <145495432+jamiedatty@users.noreply.github.com>
2026-08-08 17:39:12 +0100

    fix

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,18 @@
 <script setup>
+import { onMounted } from "vue";
 import Header from "@/components/Header.vue";
+import ErrorToast from "@/components/ErrorToast.vue";
+import { useAuth } from "@/composables/useAuth";
+
+const { fetchCurrentUser } = useAuth();
+
+onMounted(() => {
+  fetchCurrentUser();
+});
 </script>
 
 <template>
+  <ErrorToast />
   <router-view />
 </template>
 
